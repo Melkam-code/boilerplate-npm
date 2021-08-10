1f86a29184df96d781310542215ccc354bd9ee05
melkam <melkammekonnen899@gmail.com>
2021-08-10 12:51:59 +0300

    Create get post requests using query params

--- myApp.js
+++ myApp.js
@@ -9,6 +9,22 @@ app.get('/now', function middleware(req, res, next){
   res.json({time: req.time})
 });
 
+app.get("/:word/echo", (req, res) => {
+  const { word } = req.params;
+  res.json({
+    echo: word
+  });
+});
+
+//get query param values
+app.get('/name', (req, res) => {
+  var firstName = req.query.first;
+  var lastName = req.query.last;
+  res.json(
+    { name: `${firstName} ${lastName}`}
+  )
+})
+
 //create a middleware 
 app.use(function(req, res, next) {
   var string = req.method + " " + req.path + " - " + req.ip;
